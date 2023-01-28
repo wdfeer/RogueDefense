0fc747b46a2af19f09a3a601c3b007839acbfe67
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-01-28 12:50:22 +0100

    Show the number of bleeds on the enemy

---
+++ Logic/UI/InGame/BleedContainer.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class BleedContainer : HBoxContainer
+{
+    public override void _Process(float delta)
+    {
+        int bleeds = Game.instance.enemy.bleeds.Count;
+        if (bleeds > 0)
+        {
+            Visible = true;
+            ((Label)GetNode("BleedCounter")).Text = bleeds.ToString();
+        }
+        else Visible = false;
+    }
+}
