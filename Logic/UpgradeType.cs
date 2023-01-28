b874a26658d47cac74a15edb11206e1d4f2a4bab
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-01-28 08:58:34 +0100

    Fix Upgrades not applying

--- Logic/UpgradeType.cs
+++ Logic/UpgradeType.cs
@@ -20,19 +20,19 @@ namespace RogueDefense
         public Func<bool> canBeRolled = () => true;
 
         public int uniqueId;
-        public static UpgradeType MaxHp => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Max Hp");
-        public static UpgradeType DamageReduction => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Damage Reduction") { valueMult = 0.5f };
-        public static UpgradeType Damage => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Damage");
-        public static UpgradeType FireRate => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Fire Rate");
-        public static UpgradeType Multishot => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Multishot");
-        public static UpgradeType CritChance => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Crit Chance");
-        public static UpgradeType CritDamage => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Crit Damage");
-        public static UpgradeType BleedChance => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Bleed Chance");
-        public static UpgradeType AbilityStrength => new UpgradeType(x => $"+{ToPercentAndRound(x)}% Ability Strength") { valueMult = 2f };
-        public static UpgradeType BaseDamage => new UpgradeType(x => $"+{ToPercentAndRound(x)}% BASE Damage") { canBeRolled = () => Game.instance.generation % 5 == 0, chanceMult = 10f, getBaseRandomValue = () => 0.5f };
-        public static UpgradeType BaseFireRate => new UpgradeType(x => $"+{ToPercentAndRound(x)}% BASE Fire Rate") { canBeRolled = () => Game.instance.generation % 5 == 0, chanceMult = 10f, getBaseRandomValue = () => 0.5f };
-        public static UpgradeType BaseMultishot => new UpgradeType(x => $"+{ToPercentAndRound(x)}% BASE Multishot") { canBeRolled = () => Game.instance.generation % 5 == 0, chanceMult = 10f, getBaseRandomValue = () => 0.5f };
-        public static UpgradeType BaseCritMultiplier => new UpgradeType(x => $"+{ToPercentAndRound(x)}% BASE Crit Damage") { canBeRolled = () => Game.instance.generation % 5 == 0, chanceMult = 10f, getBaseRandomValue = () => 0.5f };
+        public static readonly UpgradeType MaxHp = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Max Hp");
+        public static readonly UpgradeType DamageReduction = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Damage Reduction") { valueMult = 0.5f };
+        public static readonly UpgradeType Damage = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Damage");
+        public static readonly UpgradeType FireRate = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Fire Rate");
+        public static readonly UpgradeType Multishot = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Multishot");
+        public static readonly UpgradeType CritChance = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Crit Chance");
+        public static readonly UpgradeType CritDamage = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Crit Damage");
+        public static readonly UpgradeType BleedChance = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Bleed Chance");
+        public static readonly UpgradeType AbilityStrength = new UpgradeType(x => $"+{ToPercentAndRound(x)}% Ability Strength") { valueMult = 2f };
+        public static readonly UpgradeType BaseDamage = new UpgradeType(x => $"+{ToPercentAndRound(x)}% BASE Damage") { canBeRolled = () => Game.instance.generation % 5 == 0, chanceMult = 10f, getBaseRandomValue = () => 0.5f };
+        public static readonly UpgradeType BaseFireRate = new UpgradeType(x => $"+{ToPercentAndRound(x)}% BASE Fire Rate") { canBeRolled = () => Game.instance.generation % 5 == 0, chanceMult = 10f, getBaseRandomValue = () => 0.5f };
+        public static readonly UpgradeType BaseMultishot = new UpgradeType(x => $"+{ToPercentAndRound(x)}% BASE Multishot") { canBeRolled = () => Game.instance.generation % 5 == 0, chanceMult = 10f, getBaseRandomValue = () => 0.5f };
+        public static readonly UpgradeType BaseCritMultiplier = new UpgradeType(x => $"+{ToPercentAndRound(x)}% BASE Crit Damage") { canBeRolled = () => Game.instance.generation % 5 == 0, chanceMult = 10f, getBaseRandomValue = () => 0.5f };
         public static UpgradeType[] AllTypes = new UpgradeType[] {
             MaxHp,
             DamageReduction,
