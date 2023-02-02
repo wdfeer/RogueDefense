2571e6a716ea68df8debdc281d5dfef69c488adf
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-02-02 12:16:11 +0100

    Improve SlowingField rendering

--- Logic/Enemy/SlowingField.cs
+++ Logic/Enemy/SlowingField.cs
@@ -7,6 +7,7 @@ public class SlowingField : Area2D
     {
         Visible = true;
         Connect("body_entered", this, "BodyEntered");
+        ((CircleShape2D)(GetNode("CollisionShape2D") as CollisionShape2D).Shape).Radius = radius;
     }
     public void BodyEntered(Node body)
     {
@@ -15,5 +16,11 @@ public class SlowingField : Area2D
             Bullet bullet = (Bullet)body;
             bullet.velocity *= 0.05f;
         }
+    }
+
+    public float radius = 190;
+    public override void _Draw()
+    {
+        DrawCircle(Position, radius, Color.Color8(0, 0, 25, 100));
     }
 }
