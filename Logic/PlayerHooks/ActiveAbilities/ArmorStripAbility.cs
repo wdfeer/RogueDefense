7d7a4cef992d6d4d618193b64987095377d0199f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-02-02 18:07:52 +0100

    Rebalance ArmorStripAbility

--- Logic/PlayerHooks/ActiveAbilities/ArmorStripAbility.cs
+++ Logic/PlayerHooks/ActiveAbilities/ArmorStripAbility.cs
@@ -1,3 +1,5 @@
+using Godot;
+
 namespace RogueDefense
 {
     public class ArmorStripAbility : ActiveAbility
@@ -5,35 +7,17 @@ namespace RogueDefense
         public ArmorStripAbility(CustomButton button) : base(button) { }
         public override void Activate()
         {
-            buffLeft = Duration * BASE_DURATION;
-            oldArmor = Enemy.instance.armor;
             if (Strip > 1f)
                 Enemy.instance.armor = 0;
             else
                 Enemy.instance.armor *= 1 - Strip;
             Enemy.instance.ResetArmorDisplay();
         }
-        public float oldArmor;
-        public float buffLeft = 0;
-        public override void PostUpgradeUpdate(float delta)
-        {
-            if (buffLeft > 0)
-            {
-                buffLeft -= delta;
-                if (buffLeft <= 0)
-                {
-                    Enemy.instance.armor = oldArmor;
-                    Enemy.instance.ResetArmorDisplay();
-                }
-            }
-        }
         public const float BASE_STRIP = 0.4f;
-        public const float BASE_DURATION = 5f;
-        public override float BaseCooldown => NetworkManager.Singleplayer ? 20f : 30f;
+        public override float BaseCooldown => (NetworkManager.Singleplayer ? 25f : 40f) / Mathf.Sqrt(Duration);
         public float Strip => BASE_STRIP * Strength;
         protected override string GetAbilityText()
             => $@"Remove {(int)(Strip * 100f)}% Enemy Armor
-Duration: {(BASE_DURATION * Duration).ToString("0.00")} s
 Cooldown: {Cooldown.ToString("0.00")} s";
     }
 }
