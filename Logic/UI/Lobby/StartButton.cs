f8afab73fae54d85676e8719f9833761ed1542ed
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-02-18 18:13:40 +0100

    Remove Singleplayer button

--- Logic/UI/Lobby/StartButton.cs
+++ Logic/UI/Lobby/StartButton.cs
@@ -8,6 +8,13 @@ public class StartButton : Button
         if (NetworkManager.mode != NetMode.Server)
             return;
 
-        Server.instance.SendMessage(MessageType.StartGame, new string[0]);
+        if (Client.instance.others.Count == 0)
+        {
+            NetworkManager.NetStop();
+            NetworkManager.mode = NetMode.Singleplayer;
+            Lobby.Instance.GetTree().ChangeScene("res://Scenes/Game.tscn");
+        }
+        else
+            Server.instance.SendMessage(MessageType.StartGame, new string[0]);
     }
 }
