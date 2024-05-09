8b758be73d16d82ca8cbb7dccd5fd8820a004105
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-05-09 20:10:48 +0200

    Show stages cleared on death instead of lvls

--- Logic/DefenseObjective.cs
+++ Logic/DefenseObjective.cs
@@ -111,7 +111,7 @@ namespace RogueDefense
 
             Game.instance.GetTree().Paused = true;
             DeathScreen.instance.Show();
-            (DeathScreen.instance.GetNode("ScoreLabel") as Label).Text = $"Level {Game.Wave} reached\n{PP.currentPP.ToString("0.000")} pp";
+            (DeathScreen.instance.GetNode("ScoreLabel") as Label).Text = $"{Game.instance.GetStage() - 1} Stages cleared\n{PP.currentPP.ToString("0.000")} pp";
         }
     }
 }
