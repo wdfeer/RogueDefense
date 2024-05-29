55a7de3de36131847f9b4cf5987a84e5d6535112
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-05-29 08:16:52 +0200

    Fix runtime error on launch without internet

--- Logic/UI/MainMenu/Version.gd
+++ Logic/UI/MainMenu/Version.gd
@@ -10,6 +10,9 @@ func _ready():
 	request.request("https://api.github.com/repos/wdfeer/roguedefense/releases/latest")
 
 func _on_request_completed(result, response_code, headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		return
+	
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	var latest_version: String = json["tag_name"]
 	if latest_version != text:
