7d675dbf9690d3a4dcbbeb0181fc2e56cc9a8ed5
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-09-26 14:58:16 +0200

    Fix Version label being red if latest GitHub release is lower

--- Logic/UI/MainMenu/Version.gd
+++ Logic/UI/MainMenu/Version.gd
@@ -14,8 +14,25 @@ func _on_request_completed(result, response_code, headers, body):
 		return
 	
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	var latest_version: String = json["tag_name"]
-	if latest_version != text:
+	var latest_version = json["tag_name"]
+	var current_version = text
+	if is_version_greater(latest_version, current_version):
 		print("Found a newer version " + latest_version + " (current: " + text + ")")
 		$Underline.visible = true
 		$Underline.modulate = Color.RED
+
+func is_version_greater(v1: String, v2: String):
+	if v1 == v2:
+		return false
+	
+	var numbers1 = split_version(v1)
+	var numbers2 = split_version(v2)
+	
+	for i in range(len(numbers1)):
+		if numbers1[i] < numbers2[i]:
+			return false
+	
+	return true
+
+func split_version(version: String) -> Array :
+	return (version.substr(1).split(".") as Array).map(func (str): return int(str))
