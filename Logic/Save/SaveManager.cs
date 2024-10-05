7f94b003809634a905b57dd1fb3848704d1a4b36
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-10-05 14:07:47 +0200

    Fix Exception if file not found

--- Logic/Save/SaveManager.cs
+++ Logic/Save/SaveManager.cs
@@ -1,3 +1,4 @@
+#nullable enable
 using System.Text.Json;
 
 namespace RogueDefense.Logic.Save;
@@ -9,8 +10,8 @@ public static class SaveManager
     
     public static void Load()
     {
-        user = DeserializeStruct<UserData>(UserData.PATH);
-        client = DeserializeStruct<ClientSettings>(ClientSettings.PATH);
+        user = DeserializeStruct<UserData>(UserData.PATH) ?? new();
+        client = DeserializeStruct<ClientSettings>(ClientSettings.PATH) ?? new();
     }
     
     public static void Save()
@@ -19,7 +20,7 @@ public static class SaveManager
         SerializeStruct(client, ClientSettings.PATH);
     }
 
-    private static void SerializeStruct<T>(T obj, string path)
+    private static void SerializeStruct<T>(T obj, string path)  where T : struct
     {
         FileAccess fileAccess = FileAccess.Open(path, FileAccess.ModeFlags.Write);
         using (fileAccess)
@@ -28,9 +29,10 @@ public static class SaveManager
         }
     }
     
-    private static T DeserializeStruct<T>(string path)
+    private static T? DeserializeStruct<T>(string path) where T : struct
     {
         FileAccess fileAccess = FileAccess.Open(path, FileAccess.ModeFlags.Read);
+        if (fileAccess == null) return null;
         using (fileAccess)
         {
             return JsonSerializer.Deserialize<T>(fileAccess.GetAsText());
