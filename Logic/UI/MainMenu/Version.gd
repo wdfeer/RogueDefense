6eeb9366f4625cb3f3c67581a0b83d5afb6af968
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-04-12 13:10:56 +0200

    Fix making a request to github API every main menu load

--- Logic/UI/MainMenu/Version.gd
+++ Logic/UI/MainMenu/Version.gd
@@ -1,32 +1,37 @@
 extends LinkButton
 
+static var latest_version: String = ""
+
 @onready var request: HTTPRequest = $HTTPRequest
 
 func _ready():
 	text = "v" + ProjectSettings.get_setting("application/config/version")
 	
-	request.request_completed.connect(_on_request_completed)
-	
-	request.request("https://api.github.com/repos/wdfeer/roguedefense/releases/latest")
+	if latest_version == "":
+		request.request_completed.connect(_on_request_completed)
+		request.request("https://api.github.com/repos/wdfeer/roguedefense/releases/latest")
+	_update_latest_version_visual()
+
+func _update_latest_version_visual():
+	var current_version = text
+	if _is_version_greater(latest_version, current_version):
+		print("Found a newer version " + latest_version + " (current: " + text + ")")
+		$Underline.visible = true
+		$Underline.modulate = Color.RED
 
 func _on_request_completed(result, _response_code, _headers, body):
 	if result != HTTPRequest.RESULT_SUCCESS:
 		return
 	
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	var latest_version = json["tag_name"]
-	var current_version = text
-	if is_version_greater(latest_version, current_version):
-		print("Found a newer version " + latest_version + " (current: " + text + ")")
-		$Underline.visible = true
-		$Underline.modulate = Color.RED
+	latest_version = json["tag_name"]
 
-func is_version_greater(v1: String, v2: String):
+func _is_version_greater(v1: String, v2: String):
 	if v1 == v2:
 		return false
 	
-	var numbers1 = split_version(v1)
-	var numbers2 = split_version(v2)
+	var numbers1 = _split_version(v1)
+	var numbers2 = _split_version(v2)
 	
 	for i in range(len(numbers1)):
 		if numbers1[i] < numbers2[i]:
@@ -34,5 +39,5 @@ func is_version_greater(v1: String, v2: String):
 	
 	return true
 
-func split_version(version: String) -> Array :
+func _split_version(version: String) -> Array :
 	return (version.substr(1).split(".") as Array).map(func (s): return int(s))
