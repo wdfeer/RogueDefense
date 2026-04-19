25c2ec9c1abf4b21289eac8d37d608120967095c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-04-19 13:10:00 +0200

    Make 10+ burn stacks halve enemy armor

--- Logic/Enemy/Statuses/Burn.cs
+++ Logic/Enemy/Statuses/Burn.cs
@@ -2,4 +2,11 @@ namespace RogueDefense.Logic.Enemy.Statuses;
 
 public class Burn : DamagingStatus
 {
+    public override void Process(float delta)
+    {
+        base.Process(delta);
+
+        if (Count >= 10)
+            enemy.dynamicArmorMult *= 0.5f;
+    }
 }
