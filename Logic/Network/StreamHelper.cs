fb8013922a5452e050ad0bd8a2f861316830656c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-05-09 17:06:27 +0200

    Fix errors on lobby creation

--- Logic/Network/StreamHelper.cs
+++ Logic/Network/StreamHelper.cs
@@ -7,12 +7,12 @@ public static class StreamHelper
     public static void WriteIntoStream(StreamPeer stream, MessageType type, Resource message)
     {
         stream.Put8((sbyte)type);
-        stream.PutVar(message);
+        stream.PutVar(message, true);
     }
     public static Tuple<MessageType, Resource> ReadFromStream(StreamPeer stream)
     {
         MessageType type = (MessageType)stream.Get8();
-        Resource message = (Resource)stream.GetVar();
-        return new Tuple<MessageType, Resource>(type, message);
+        Variant obj = stream.GetVar(true);
+        return new Tuple<MessageType, Resource>(type, (Resource)obj);
     }
 }
